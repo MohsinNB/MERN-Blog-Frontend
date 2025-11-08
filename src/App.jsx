22c8109e18c2d27dc0c9c44c2a987566e740468f
Mohsin NB <mohammadmohsin28012005@gmail.com>
2025-11-08 19:41:24 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar/Navbar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Home></Home>
+      </>
+    ),
+  },
+  {
+    path: "/blogs",
+    element: (
+      <>
+        <Navbar />
+        <Blogs />
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <Navbar />
+        <About />
+      </>
+    ),
+  },
+
+  {
+    path: "/login",
+    element: (
+      <>
+        <Navbar />
+        <Login />
+      </>
+    ),
+  },
+
+  {
+    path: "/Signup",
+    element: (
+      <>
+        <Navbar />
+        <Signup />
+      </>
+    ),
+  },
+]);
+
+const App = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
