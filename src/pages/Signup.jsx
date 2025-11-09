bc08610f6f11dc433f6fc9ca91449cbeec9b9560
Mohammad Mohsin <mohammadmohsin28012005@gmail.com>
2025-11-09 08:21:01 +0600

    Add existing code of signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import auth from "../assets/auth.jpg";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="flex h-screen md:pt-14 md:h-[760px]">
+      <div className="hidden md:block">
+        <img className="h-[700px]" src={auth} alt="" />
+      </div>
+      {/* signup form */}
+      <div className="flex justify-center items-center flex-1 px-4 md:px-0">
+        <Card className="w-full max-w-md p-6 shadow-lg rounded-2xl dark:bg-gray-800 dark:border-gray-600">
+          <CardHeader>
+            <CardTitle>
+              <h1 className="text-center text-xl font-semibold">
+                Create an account
+              </h1>
+            </CardTitle>
+            <p className="mt-2 text-sm font-serif text-center dark:text-gray-300">
+              Enter your details below to create an account
+            </p>
+          </CardHeader>
+          <CardContent>
+            <form className="space-y-4">
+              <div className="flex gap-3">
+                <div>
+                  <Label>First Name</Label>
+                  <Input
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    className="dark:border-gray-600 dark:bg-gray-900"
+                  />
+                </div>
+                <div>
+                  <Label>Last Name</Label>
+                  <Input
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    className="dark:border-gray-600 dark:bg-gray-900"
+                  />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
